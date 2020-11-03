318de8c133119a279d04a12b38357c6f0ac5cef2
Vikram Pal <vikramsonupal@gmail.com>
2020-11-03 11:20:37 -0800

    Worked through additional backend functionality

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
 
 const PORT = 3000;
 
 const publicDirectory = path.join(__dirname, '../public');
-
 app.use(express.static(publicDirectory));
+
+mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true} );
 
 app.listen(PORT);
